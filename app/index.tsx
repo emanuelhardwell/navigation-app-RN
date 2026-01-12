85c66e4719fa21a5f9a081c00322d9b3dba8beba
emanuelhardwell <emanuelnoe16@gmail.com>
2026-01-11 20:10:53 -0600

    feat: Config the fonts family WorkSans

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="mt-20">
-      <Text className="text-3xl color-blue-900 text-center">App</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-5 mx-3">
+        <Text className="text-5xl color-blue-500 font-work-black">App</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
