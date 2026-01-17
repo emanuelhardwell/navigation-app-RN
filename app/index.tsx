41d4a130b9e288d6db721374716d3379b319690a
emanuelhardwell <emanuelnoe16@gmail.com>
2026-01-17 12:34:48 -0600

    feat: Config tab navigation

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 const App = () => {
-  return <Redirect href={"/home"} />;
+  /*   return <Redirect href={"/home"} />;
+   */ return <Redirect href={"/tab/home"} />;
   /* return (
     <SafeAreaView>
       <View className="mt-5 mx-3">
